77d276f862a6b2df9a1d198a9487e0d78147e091
tvmcoder <anandupn@outlook.com>
2024-12-09 13:39:43 +0530

    09-12-2024 qn5-Publisher

--- bookPubisher.py
+++ bookPubisher.py
@@ -6,14 +6,17 @@ class book(publisher):
         super().__init__(name)
         self.title=title
         self.author=author
-class python(publisher):
+class python(book):
     def __init__(self,name,title,author,price,pages):
-        super().__init__(self,name,title,author)
+        super().__init__(name,title,author)
         self.price=price
         self.pages=pages
-    def display():
+    def display(self):
         print(f"Publisher: {self.name}") 
         print(f"Title: {self.title}") 
         print(f"Author: {self.author}") 
         print(f"Price: {self.price}") 
-        print(f"Number of Pages: {self.num_pages}")
+        print(f"Number of Pages: {self.pages}")
+
+python_book = python("O'Reilly Media", "Learning Python", "Mark Lutz", 45.99, 1594) 
+python_book.display()
